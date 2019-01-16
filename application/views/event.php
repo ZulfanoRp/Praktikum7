b9f399d09916c7f0584916923240dd160b93bbc4
zulfano <zulfano67@gmail.com>
2019-01-16 13:22:25 +0700

    selesai

---
+++ application/views/event.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <title>Profil Admin</title>
+</head>
+<body style="margin: 175px; font-family: monospace; font-style: italic; font-size: 5mm; background-color: #2378cc;text-align: center">
+    <img style="width: 200px;" src="<?=base_url()?>assets/munchen.jpg" alt="">
+    <h1>Diskon 50% </h1>
+    <p>Dapatkan Diskon Setengah Harga bagi 10 tercepat membeli di Sukun</p>
+    <p>Minggu,10 October 2020</p>
+</body>
+</html>
---
+++ application/views/gallery.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profil admin</title>
+    <style>
+        body{
+            margin:51pxpx;
+            font-family:'Century Gothic',sans-serif;
+            background-color:#ededed;
+            text-align:center;
+        }
+    .judul{
+        margin-bottom:51px;
+        font-size:larger;
+        margin-top:35px;
+    }
+    </style>
+</head>
+<body>
+    <p class="judul">Koleksi sepatu sukun malang </p>
+    <img style="height:200 ;width:400px;"src="<?=base_url()?>assets/tahiti.png" alt="">
+    <p>Adidas Tahiti</p>
+    <img style="height:200 ;width:400px;"src="<?=base_url()?>assets/jamaica.jpg" alt="">
+    <p>Adidas Jamaica</p>
+    <img style="height:200 ;width:400px;"src="<?=base_url()?>assets/spezial.jpg" alt="">
+    <p>Adidas Spezial</p>
+    <img style="height:200 ;width:400px;"src="<?=base_url()?>assets/munchen.jpg" alt="">
+    <p>Adidas Munchen</p>
+</body>
+</html>
